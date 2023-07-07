4524a1d0a28d8b1335fa84e68e7a97c6cedc0863
Mohamed Elsayed <mohamedelsayed20258@gmail.com>
2023-07-07 05:53:51 -0700

    dd

---
+++ index.js
@@ -0,0 +1,13 @@
+
+import express from 'express'
+
+
+const app = express()
+
+
+app.get('/' , (req,res) => {
+    res.send('<h1>Mohamed Elsayed </h1>')
+})
+
+
+app.listen(3005 , () => console.log('listening server'))
